199b583ee0a0b23c5deff07cad4b0de95be85dc5
cloudenum <23111954+cloudenum@users.noreply.github.com>
2020-07-08 19:37:45 +0700

    hufffttt

--- application/libraries/Password_hash.php
+++ application/libraries/Password_hash.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class password_hash {
+class Password_hash {
 	private CI_Controller $_CI;
 	private CI_Encryption $_encryption;
 
